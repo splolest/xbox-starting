fab58d185479142cb07bae82dab2a63b9e3b9131
ClarenceHetu-Dufresne <https://users.github@splolest.com>
2020-03-01 11:53:12 -0500

    Ajoutez des fichiers projet.

---
+++ xbox starting/MainPage.xaml.vb
@@ -0,0 +1,9 @@
+﻿' Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
